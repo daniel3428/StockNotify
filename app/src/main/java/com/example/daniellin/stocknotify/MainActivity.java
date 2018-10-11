b5ea2826020d01b73988c1dba919c4ecf217d75f
daniel3428 <daniel153282028@gmail.com>
2018-10-11 22:39:39 +0800

    add start button

--- app/src/main/java/com/example/daniellin/stocknotify/MainActivity.java
+++ app/src/main/java/com/example/daniellin/stocknotify/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.daniellin.stocknotify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startStockTracing(View view) {
+        Toast toast = Toast.makeText(this, "按鈕已經被點擊", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
